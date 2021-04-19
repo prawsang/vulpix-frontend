810e622f6686d1ed348ef529bc7355f7f35962a8
prawsang <prawsang.c@gmail.com>
2021-04-20 00:24:18 +0700

    fetch categories from db

--- components/browse/CategorySelect.tsx
+++ components/browse/CategorySelect.tsx
@@ -1,22 +1,37 @@
 // import { useState } from 'react'
 import { Box, Grid, Heading, BoxProps } from '@chakra-ui/react'
+import { getCategories } from 'api/browse'
 import CategoryCard from 'components/browse/CategoryCard'
+import { useEffect, useState } from 'react'
+import { Category } from 'types/common'
 
-const Home = (props: BoxProps) => {
+const CategorySelect = (props: BoxProps) => {
+  const [categories, setCategories] = useState<Category[]>([])
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const res = await getCategories()
+      if (res && res.data) setCategories(res.data)
+    }
+    fetchCategories()
+  }, [])
+
   return (
     <Box {...props}>
       <Heading size="sm" color="gray.600" mb="32px" textAlign="center" textTransform="uppercase">
         Browse By Category
       </Heading>
       <Grid templateColumns="repeat(4, 1fr)" gap={6}>
-        <CategoryCard name="Social Networking" link="/browse/category/social-networking" />
-        <CategoryCard name="Games" link="/browse/category/games" />
-        <CategoryCard name="Lifestyle" link="/browse/category/lifestyle" />
-        <CategoryCard name="Photography" link="/browse/category/photography" />
-        <CategoryCard name="Finance" link="/browse/category/finance" />
+        {categories.map((cat) => (
+          <CategoryCard
+            name={cat.name}
+            link={`/browse/category/${cat.slug}`}
+            key={`${cat.slug}-select`}
+          />
+        ))}
       </Grid>
     </Box>
   )
 }
 
-export default Home
+export default CategorySelect
