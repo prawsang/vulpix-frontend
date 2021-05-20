8ea98490882868bd0a24766527d5f7395d156706
prawsang <prawsang.c@gmail.com>
2021-05-21 04:07:17 +0700

    add email for testing

--- api/testing.ts
+++ api/testing.ts
@@ -4,10 +4,11 @@ import axios from 'axios'
 
 const url = `${process.env.NEXT_PUBLIC_TESTING_BACKEND_URL}/api`
 
-export const sendTestingRequest = async (identifier) => {
+export const sendTestingRequest = async (identifier: string, email: string) => {
   const res = await axios
     .post(`${url}/tests`, {
       application_id: identifier,
+      requester_email: email,
       forced: false,
     })
     .catch((err) => console.log(err))
